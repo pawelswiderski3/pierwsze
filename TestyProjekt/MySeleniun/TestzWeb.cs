e8da54042e1dbb80db61aab472321a4e4f888b22
pawelswiderski3 <pawel.swiderski3@gmail.com>
2017-11-28 17:27:14 +0100

    balagan

---
+++ TestyProjekt/MySeleniun/TestzWeb.cs
@@ -0,0 +1,231 @@
+﻿using System;
+
+using System.Text;
+
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Firefox;
+
+using OpenQA.Selenium.Support.UI;
+
+using Xunit;
+
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SeleniumTests
+
+{
+
+    public class Example : IDisposable
+
+    {
+
+        private IWebDriver driver;
+
+        private StringBuilder verificationErrors;
+
+        private string baseURL;
+
+        private bool acceptNextAlert = true;
+
+
+
+        public Example()
+
+        {
+
+           driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            baseURL = "https://www.google.pl/";
+
+            verificationErrors = new StringBuilder();
+
+        }
+
+
+        
+
+
+
+
+        [Fact]
+
+        public void TheExampleTest()
+
+        {
+
+            driver.Navigate().GoToUrl(baseURL);
+
+            driver.FindElement(By.Id("lst-ib")).Clear();
+            driver.FindElement(By.Id("lst-ib")).SendKeys("code sprinters");
+            driver.FindElement(By.Id("lst-ib")).Submit();
+
+            driver.FindElement(By.LinkText("Code Sprinters -")).Click();
+
+            var element = driver.FindElement(By.LinkText("Poznaj nasze podejście"));
+
+            Assert.NotNull(element);
+
+
+            var elements = driver.FindElements(By.LinkText("Poznaj nasze podejście"));
+
+            Assert.Single(elements);
+
+           
+            Thread.Sleep(3000);
+            WaitForClickable(By.LinkText("Akceptuję"), 1);
+
+            driver.FindElement(By.LinkText("Akceptuję")).Click();
+            ;
+
+            WaitForClickable(By.LinkText("Poznaj nasze podejście"), 2);
+
+            driver.FindElement(By.LinkText("Poznaj nasze podejście")).Click();
+
+
+            Assert.NotNull(elements);
+
+            protected void waitForClickable(By by, int seconds)
+
+            {
+
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+
+                wait.Until(ExpectedConditions.ElementToBeClickable(by));
+
+            }
+
+        //private void WaitForClickable(By by, int v)
+        {
+      //      throw new NotImplementedException();
+        }
+
+        //private bool IsElementPresent(By by)
+
+        //{
+
+        //    try
+
+        //    {
+
+        //        driver.FindElement(by);
+
+        //        return true;
+
+        //    }
+
+        //    catch (NoSuchElementException)
+
+        //    {
+
+        //        return false;
+
+        //    }
+
+        //}
+
+
+
+        //private bool IsAlertPresent()
+
+        //{
+
+        //    try
+
+        //    {
+
+        //        driver.SwitchTo().Alert();
+
+        //        return true;
+
+        //    }
+
+        //    catch (NoAlertPresentException)
+
+        //    {
+
+        //        return false;
+
+        //    }
+
+        //}
+
+
+
+        //private string CloseAlertAndGetItsText()
+
+        //{
+
+        //    try
+
+        //    {
+
+        //        IAlert alert = driver.SwitchTo().Alert();
+
+        //        string alertText = alert.Text;
+
+        //        if (acceptNextAlert)
+
+        //        {
+
+        //            alert.Accept();
+
+        //        }
+
+        //        else
+
+        //        {
+
+        //            alert.Dismiss();
+
+        //        }
+
+        //        return alertText;
+
+        //    }
+
+        //    finally
+
+        //    {
+
+        //        acceptNextAlert = true;
+
+        //    }
+
+        //}
+
+
+
+        public void Dispose()
+
+        {
+
+            try
+
+            {
+
+                driver.Quit();
+
+            }
+
+            catch (Exception)
+
+            {
+
+                // Ignore errors if unable to close the browser
+
+            }
+
+            Assert.Equal("", verificationErrors.ToString());
+
+        }
+
+    }
+
+        private void WaitForClickable(By by, int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
