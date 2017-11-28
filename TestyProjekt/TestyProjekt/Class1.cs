5a660a0bac57de764559958b8181ba26c67302d5
pawelswiderski3 <pawel.swiderski3@gmail.com>
2017-11-28 11:25:44 +0100

    kolejny commit

--- TestyProjekt/TestyProjekt/Class1.cs
+++ TestyProjekt/TestyProjekt/Class1.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace TestyProjekt
 {
-    public class Class1
+    public class Matma
     {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
     }
 }
